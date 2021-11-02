8cc7e55d9fa24cca0d1577e83481762b5cfe2f42
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-02 22:24:05 +0300

    Utility function: A connection to the database

---
+++ models/setup.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var DB *gorm.DB
+
+/*
+   This is a utility function to connect us to the database
+*/
+
+func ConnectDatabase() {
+	database, err := gorm.Open("sqlite3", "test.db")
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&Book{})
+
+	DB = database
+}
